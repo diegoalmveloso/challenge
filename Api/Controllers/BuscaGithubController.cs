a8afcb524fa6e8f7965cd2c8453e95e94e870de6
Diego Almeida Veloso <diego.almeida@eteg.com.br>
2021-10-30 16:04:04 -0300

    Criação da API e dos comandos básicos
    Criação do esqueleto da API em C#
    Base da requisição Get feita, mas ainda não funcional

---
+++ Api/Controllers/BuscaGithubController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using githubResponse.Models
+
+namespace api.Controller
+{
+	[Api Controller]
+	[Route("v1/blipjson")]
+	
+	public class BuscaGithubController
+	{
+		[HttpGet]
+		[Route("")]
+		Task<GithubResponser> GetAddressAsync();
+	}
+}
---
+++ Api/Models/GithubResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace githubResponse.Models
+{
+    public class GithubResponse
+    {
+        [JsonProperty("avatar")]
+        public string Avatar { get; set; }
+        [JsonProperty("tirulo_repositorio")]
+        public string TituloRepositorio { get; set; }
+        [JsonProperty("subtitulo_repositorio")]
+        public string SubtituloRepositorio { get; set; }
+        [JsonProperty("data_criacao")]
+        public string DataCriacaoRepositorio { get; set; }
+        [JsonProperty("language_repositorio")]
+        public string LanguageRepositorio { get; set; }
+    }
+}
